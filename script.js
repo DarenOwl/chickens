abca1f6f1dc737539d6153641475372e8fcf3b5f
DarenOwl <DarenOwl@users.noreply.github.com>
2020-04-10 00:00:39 +0500

    third person

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var squares;
 var sources =
   {
     hen:      './tex/hen.png',
-    cock:      './tex/cock.png'
+    cock:     './tex/cock.png',
+    wife:     './tex/wife.png',
   };
 
 var images = {};
@@ -18,7 +19,7 @@ function Init() {
     context = canvas.getContext("2d");
 
     //squares
-    squares = new Squares(canvas,8,256);
+    squares = new Squares(canvas,10,256);
 
     //картинки
     for(var name in sources)
@@ -28,12 +29,14 @@ function Init() {
     }
 
     squares.setOnClick(function clickAt(x,y) {
-        console.log("click from script.js");
+        console.log("clicked square " + x + "; " + y);
         if (x == hen.x && y == hen.y){
             chosen = hen;
         } else if (x == cock.x && y == cock.y) {
             chosen = cock;
-        } else if (chosen != null && Math.abs(chosen.x - x) + Math.abs(chosen.y - y) < 2){
+        } else if (x == wife.x && y == wife.y) {
+              chosen = wife;
+        } else if (chosen != null && Math.abs(chosen.x - x) + Math.abs(chosen.y - y) < 2 && x > 0 && x < 9 && y > 0 && y < 9){
           chosen.x = x;
           chosen.y = y;
           chosen = null;
@@ -42,14 +45,19 @@ function Init() {
 }
 
 var hen = {
-  x: 0,
-  y: 0
+  x: 8,
+  y: 4
 };
 
 var cock = {
-  x: 3,
-  y: 2
+  x: 1,
+  y: 4
 };
+
+var wife = {
+  x: 3,
+  y: 6
+}
 
 var chosen = hen;
 
@@ -60,18 +68,25 @@ document.addEventListener("DOMContentLoaded", function ()
     {
         squares.clearCanvas();
         if (chosen != null){
-            squares.underlineSquare(chosen.x, chosen.y);
-            squares.underlineSquare(chosen.x - 1, chosen.y, 'yellow');
-            squares.underlineSquare(chosen.x + 1, chosen.y, 'yellow');
-            squares.underlineSquare(chosen.x, chosen.y - 1, 'yellow');
-            squares.underlineSquare(chosen.x, chosen.y + 1, 'yellow');
+            drawSquaresAround(chosen.x,chosen.y,'#DED274');
             squares.underlineMouseSquare();
         }
         squares.drawImage(images.hen, hen.x, hen.y);
         squares.drawImage(images.cock, cock.x, cock.y - 0.5, 1, 1.5);
+        squares.drawImage(images.wife, wife.x - 0.5, wife.y - 2, 2, 3);
     }, 100);
 });
 
+function drawSquaresAround(x,y,color) {
+    if (x - 1 > 0)
+        squares.underlineSquare(x - 1, y, color);
+    if (x + 1 < 9)
+        squares.underlineSquare(x + 1, y, color);
+    if (y - 1 > 0)
+        squares.underlineSquare(x, y - 1, color);
+    if (y + 1 < 9)
+        squares.underlineSquare(x, y + 1, color);
+}
 
 //SOME HELP FUNCTIONS
 
